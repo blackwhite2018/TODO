73e9c496d6fa79474d1a7fce345206e8d1a9cfa0
Sergei <black_white2018@rambler.ru>
2020-08-26 01:30:54 +0300

    Updates

---
+++ precache-manifest.f99c1b37e8bc9caa5f53bb6d97cf3c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5acb2e67db832ba9f3a6288574d23c",
+    "url": "/blackwhite2018/TODO/index.html"
+  },
+  {
+    "revision": "8845ba8ed2cfe5407230",
+    "url": "/blackwhite2018/TODO/static/css/main.917424d2.chunk.css"
+  },
+  {
+    "revision": "ea4a975e9ce4ad98f22a",
+    "url": "/blackwhite2018/TODO/static/js/2.0cbf578f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blackwhite2018/TODO/static/js/2.0cbf578f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8845ba8ed2cfe5407230",
+    "url": "/blackwhite2018/TODO/static/js/main.8f6b2f4a.chunk.js"
+  },
+  {
+    "revision": "c3ee30e583d0588ee2d8",
+    "url": "/blackwhite2018/TODO/static/js/runtime-main.33e59ed1.js"
+  }
+]);
